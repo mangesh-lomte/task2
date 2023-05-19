8c2975e41243ecfa2919220aa8d122999a91deba
mangesh-lomte <97304542+mangesh-lomte@users.noreply.github.com>
2023-05-19 17:26:16 +0530

    first commit

---
+++ t44444.js
@@ -0,0 +1,16 @@
+// Function to append a value to the expression
+function appendToExpression(value) {
+    const expression = document.getElementById('expression');
+    expression.value += value;
+  }
+  
+  // Function to clear the expression
+  function clearExpression() {
+    const expression = document.getElementById('expression');
+    expression.value = '';
+  }
+  
+  // Function to calculate the expression
+  function calculateExpression() {
+    const expression = document.getElementById
+  
